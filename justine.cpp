66f8b155e32636683e34707a59fcf1215748635d
justineNjorg <realinjustinekeith@gmail.com>
2024-09-12 09:07:39 +0800

    Add files via upload

---
+++ justine.cpp
@@ -0,0 +1,7 @@
+#include <iostream> // Include the input-output stream library
+
+int main() {
+    std::cout << "Hello, World!" << std::endl; // Output "Hello, World!" to the console
+    return 0; // Return 0 to indicate successful execution
+}
+
